03d686a6c0b088f47d98b39ef628f4de7cec9775
Chris Norman <chris.norman2@googlemail.com>
2022-10-11 19:30:54 +0100

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+/// The top-level app class.
+class MyApp extends StatelessWidget {
+  /// Create an instance.
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(final BuildContext context) => MaterialApp(
+        title: 'Crossbow',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(),
+      );
+}
