51af39039c8220805f1c026bba18c85bc1a4abe5
Chris Norman <chris.norman2@googlemail.com>
2022-10-13 22:52:49 +0100

    Added project functions.

--- lib/src/json/project.dart
+++ lib/src/json/project.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import 'project_function.dart';
+
 part 'project.g.dart';
 
 /// The top-level project class.
@@ -10,7 +12,8 @@ class Project {
     this.name = 'Untitled Project',
     this.appName = 'untitled_project',
     this.orgName = 'com.example',
-  });
+    final List<ProjectFunction>? functions,
+  }) : functions = functions ?? [];
 
   /// Create an instance from a JSON object.
   factory Project.fromJson(final Map<String, dynamic> json) =>
@@ -24,6 +27,9 @@ class Project {
 
   /// The org name for this project.
   String orgName;
+
+  /// The functions which have been defined.
+  List<ProjectFunction> functions;
 
   /// Convert an instance to JSON.
   Map<String, dynamic> toJson() => _$ProjectToJson(this);
--- lib/src/json/project.g.dart
+++ lib/src/json/project.g.dart
@@ -10,10 +10,14 @@ Project _$ProjectFromJson(Map<String, dynamic> json) => Project(
       name: json['name'] as String? ?? 'Untitled Project',
       appName: json['appName'] as String? ?? 'untitled_project',
       orgName: json['orgName'] as String? ?? 'com.example',
+      functions: (json['functions'] as List<dynamic>?)
+          ?.map((e) => ProjectFunction.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$ProjectToJson(Project instance) => <String, dynamic>{
       'name': instance.name,
       'appName': instance.appName,
       'orgName': instance.orgName,
+      'functions': instance.functions,
     };
