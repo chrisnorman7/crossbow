86693701a97c1e8650d2698659bff62e0f1c96a7
Chris Norman <chris.norman2@googlemail.com>
2023-02-16 10:25:35 +0000

    Shortened the example.

--- crossbow_backend/example/crossbow_backend_example.dart
+++ crossbow_backend/example/crossbow_backend_example.dart
@@ -12,33 +12,18 @@ Future<void> main() async {
   await menus.createMenuItem(menuId: menu.id, name: 'Play Game');
   final stopGame = await db.stopGamesDao.createStopGame(after: 3000);
   final popLevel = await db.popLevelsDao.createPopLevel(fadeLength: 3.0);
-  final command = await db.commandsDao.setPopLevel(
-    commandID: (await db.commandsDao.setMessageText(
-      commandId: (await db.commandsDao.setStopGame(
-        commandId: (await db.commandsDao.createCommand()).id,
-        stopGameId: stopGame.id,
-      ))
-          .id,
-      text: 'The game will now close.',
-    ))
-        .id,
-    popLevelId: popLevel.id,
-  );
-  await menus.setCallCommand(
-    menuItemId: (await menus.createMenuItem(
-      menuId: menu.id,
-      name: 'Quit Game',
-      position: 1,
-    ))
-        .id,
-    callCommandId: (await db.callCommandsDao.createCallCommand(
-      commandId: command.id,
-    ))
-        .id,
-  );
   final commands = db.commandsDao;
-  await commands.setMessageText(
-    commandId: projectContext.project.initialCommandId,
+  final quitCommand = await commands.createCommand(
+    messageText: 'The game will now close.',
+    popLevelId: popLevel.id,
+    stopGameId: stopGame.id,
+  );
+  final quitCallCommand =
+      await db.callCommandsDao.createCallCommand(commandId: quitCommand.id);
+  await menus.createMenuItem(
+    menuId: menu.id,
+    name: 'Quit Game',
+    callCommandId: quitCallCommand.id,
   );
   final pushMenu = await db.pushMenusDao.createPushMenu(menuId: menu.id);
   await commands.setPushMenu(
