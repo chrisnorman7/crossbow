41c7fe1e8020e82a06f29286d2deca0c1122da06
Chris Norman <chris.norman2@googlemail.com>
2023-02-24 10:27:33 +0000

    Fix up the build script.

--- crossbow_backend/bin/build_game.dart
+++ crossbow_backend/bin/build_game.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: avoid_print
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:crossbow_backend/crossbow_backend.dart';
 import 'package:dart_style/dart_style.dart';
-import 'package:encrypt/encrypt.dart';
 import 'package:jinja/jinja.dart';
 import 'package:path/path.dart' as path;
 import 'package:recase/recase.dart';
+import 'package:ziggurat/util.dart';
 import 'package:ziggurat/ziggurat.dart' as ziggurat;
 
 const code = '''
@@ -34,54 +35,50 @@ Future<void> main(final List<String> args) async {
       'You must specify a project file to load and an output directory.',
     );
   }
-  final projectFilename = args.first;
-  final outputDirectoryName = args.last;
-  print('Importing project $projectFilename into $outputDirectoryName.');
-  var projectContext = ProjectContext.fromFile(File(projectFilename));
-  final outputDirectory = Directory(outputDirectoryName);
-  if (outputDirectory.existsSync()) {
+  final oldProjectFile = File(args.first);
+  final oldProjectDirectory = oldProjectFile.parent;
+  final newProjectDirectory = Directory(args.last);
+  print(
+    'Importing project ${oldProjectFile.path} into '
+    '${newProjectDirectory.path}.',
+  );
+  if (newProjectDirectory.existsSync()) {
     return print(
-      'The output directory ${outputDirectory.path} already exists.',
+      'The output directory ${newProjectDirectory.path} already exists.',
     );
   }
-  outputDirectory.createSync(recursive: true);
+  newProjectDirectory.createSync(recursive: true);
+  final project =
+      Project.fromJson(jsonDecode(oldProjectFile.readAsStringSync()));
   final assetsDirectory = Directory(
-    path.join(outputDirectory.path, projectContext.project.assetsDirectory),
+    path.join(newProjectDirectory.path, project.assetsDirectory),
   )..createSync(recursive: true);
   print('Copying database...');
-  final databaseFilename = projectContext.project.databaseFilename;
-  final dbFile = File(
+  final oldDatabaseFile = File(
     path.join(
-      projectContext.projectDirectory.path,
-      databaseFilename,
+      oldProjectFile.parent.path,
+      project.databaseFilename,
     ),
   );
-  File(path.join(outputDirectoryName, databaseFilename))
-      .writeAsBytesSync(dbFile.readAsBytesSync());
+  final newDatabaseFile =
+      File(path.join(newProjectDirectory.path, project.databaseFilename))
+        ..writeAsBytesSync(oldDatabaseFile.readAsBytesSync());
   print('Importing project...');
-  final encryptionKey = SecureRandom(32).base64;
-  final key = Key.fromBase64(encryptionKey);
-  final iv = IV.fromLength(16);
-  final encrypter = Encrypter(AES(key));
-  final data = encrypter
-      .encryptBytes(projectContext.file.readAsBytesSync(), iv: iv)
-      .bytes;
-  final filename = path.join(outputDirectory.path, 'project.encrypted');
-  final newProjectFile = File(filename)..writeAsBytesSync(data);
-  print('Encryption key: $encryptionKey');
-  print('Closing old database...');
-  await projectContext.db.close();
-  projectContext = ProjectContext.fromFile(
-    newProjectFile,
-    encryptionKey: encryptionKey,
+  final filename = path.join(newProjectDirectory.path, 'project.encrypted');
+  final newProjectFile = File(filename);
+  final encryptionKey = encryptFile(
+    inputFile: oldProjectFile,
+    outputFile: newProjectFile,
   );
-  final db = projectContext.db;
+  print('Encryption key: $encryptionKey');
+  print('Opening database...');
+  final db = CrossbowBackendDatabase.fromFile(newDatabaseFile);
   final encryptionKeys = <int, String>{};
   final assetStores = <String, ziggurat.AssetStore>{};
   final query = db.select(db.assetReferences);
   final oldAssetsDirectory = path.join(
-    path.dirname(projectFilename),
-    projectContext.project.assetsDirectory,
+    oldProjectDirectory.path,
+    project.assetsDirectory,
   );
   for (final assetReference in await query.get()) {
     final folderName = assetReference.folderName;
@@ -154,9 +151,9 @@ Future<void> main(final List<String> args) async {
     'assetReferenceEncryptionKeys': encryptionKeys,
   });
   final formatter = DartFormatter();
-  final projectNameSnake = projectContext.project.projectName.snakeCase;
+  final projectNameSnake = project.projectName.snakeCase;
   final outputFilename = path.join(
-    outputDirectoryName,
+    newProjectDirectory.path,
     '$projectNameSnake.dart',
   );
   File(outputFilename).writeAsStringSync(formatter.format(output));
