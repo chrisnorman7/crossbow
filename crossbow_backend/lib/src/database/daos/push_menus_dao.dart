d487ac3d2d19811ed95d198c02d2c078a85bcf2e
Chris Norman <chris.norman2@googlemail.com>
2023-02-27 10:08:56 +0000

    Added various setter methods for `PushMenu` instances.

--- crossbow_backend/lib/src/database/daos/push_menus_dao.dart
+++ crossbow_backend/lib/src/database/daos/push_menus_dao.dart
@@ -39,4 +39,39 @@ class PushMenusDao extends DatabaseAccessor<CrossbowBackendDatabase>
       ..where((final table) => table.id.equals(id));
     return query.go();
   }
+
+  /// Set the [menuId] for the push menu with the given [pushMenuId].
+  Future<PushMenu> setMenuId({
+    required final int pushMenuId,
+    required final int menuId,
+  }) async {
+    final query = update(pushMenus)
+      ..where((final table) => table.id.equals(pushMenuId));
+    return (await query
+            .writeReturning(PushMenusCompanion(menuId: Value(menuId))))
+        .single;
+  }
+
+  /// Set the [after] value for the push menu with the given [pushMenuId].
+  Future<PushMenu> setAfter({
+    required final int pushMenuId,
+    final int? after,
+  }) async {
+    final query = update(pushMenus)
+      ..where((final table) => table.id.equals(pushMenuId));
+    return (await query.writeReturning(PushMenusCompanion(after: Value(after))))
+        .single;
+  }
+
+  /// Set the [fadeLength] for the push menu with the given [pushMenuId].
+  Future<PushMenu> setFadeLength({
+    required final int pushMenuId,
+    final double? fadeLength,
+  }) async {
+    final query = update(pushMenus)
+      ..where((final table) => table.id.equals(pushMenuId));
+    return (await query
+            .writeReturning(PushMenusCompanion(fadeLength: Value(fadeLength))))
+        .single;
+  }
 }
--- crossbow_backend/test/src/database/daos/push_menus_dao_test.dart
+++ crossbow_backend/test/src/database/daos/push_menus_dao_test.dart
@@ -51,6 +51,71 @@ void main() {
           expect((await commands.getCommand(id: command.id)).pushMenuId, null);
         },
       );
+
+      test(
+        '.setMenuId',
+        () async {
+          final menu = await menus.createMenu(name: 'Test Menu');
+          final pushMenu = await pushMenus.createPushMenu(menuId: menu.id);
+          expect(pushMenu.menuId, menu.id);
+          final menu2 = await menus.createMenu(name: 'Other Menu');
+          var updatedPushMenu = await pushMenus.setMenuId(
+            pushMenuId: pushMenu.id,
+            menuId: menu2.id,
+          );
+          expect(updatedPushMenu.id, pushMenu.id);
+          expect(updatedPushMenu.menuId, menu2.id);
+          updatedPushMenu = await pushMenus.setMenuId(
+            pushMenuId: pushMenu.id,
+            menuId: menu.id,
+          );
+          expect(updatedPushMenu.id, pushMenu.id);
+          expect(updatedPushMenu.menuId, menu.id);
+        },
+      );
+
+      test(
+        '.setAfter',
+        () async {
+          final menu = await menus.createMenu(name: 'Test Menu');
+          final pushMenu =
+              await pushMenus.createPushMenu(menuId: menu.id, after: 1234);
+          expect(pushMenu.menuId, menu.id);
+          expect(pushMenu.after, 1234);
+          var updatedPushMenu =
+              await pushMenus.setAfter(pushMenuId: pushMenu.id);
+          expect(updatedPushMenu.id, pushMenu.id);
+          expect(updatedPushMenu.after, null);
+          updatedPushMenu =
+              await pushMenus.setAfter(pushMenuId: pushMenu.id, after: 4321);
+          expect(updatedPushMenu.id, pushMenu.id);
+          expect(updatedPushMenu.after, 4321);
+        },
+      );
+
+      test(
+        '.setFadeLength',
+        () async {
+          final menu = await menus.createMenu(name: 'Test Menu');
+          final pushMenu = await pushMenus.createPushMenu(
+            menuId: menu.id,
+            fadeLength: 5.0,
+          );
+          expect(pushMenu.menuId, menu.id);
+          expect(pushMenu.fadeLength, 5.0);
+          var updatedPushMenu = await pushMenus.setFadeLength(
+            pushMenuId: pushMenu.id,
+          );
+          expect(updatedPushMenu.id, pushMenu.id);
+          expect(updatedPushMenu.fadeLength, null);
+          updatedPushMenu = await pushMenus.setFadeLength(
+            pushMenuId: pushMenu.id,
+            fadeLength: 2.0,
+          );
+          expect(updatedPushMenu.id, pushMenu.id);
+          expect(updatedPushMenu.fadeLength, 2.0);
+        },
+      );
     },
   );
 }
