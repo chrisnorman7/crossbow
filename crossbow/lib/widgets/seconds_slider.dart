ea7834e6513331ce6932489af80a867d145686a9
Chris Norman <chris.norman2@googlemail.com>
2023-02-28 19:29:49 +0000

    Added the `SecondsSlider` widget.

---
+++ crossbow/lib/widgets/seconds_slider.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../messages.dart';
+
+/// A slider to set seconds.
+class SecondsSlider extends StatelessWidget {
+  /// Create an instance.
+  const SecondsSlider({
+    required this.seconds,
+    required this.onChanged,
+    this.maxSeconds = 3600,
+    super.key,
+  });
+
+  /// The number of seconds to start with.
+  final double? seconds;
+
+  /// The function to call when [seconds] change.
+  final ValueChanged<double?> onChanged;
+
+  /// The maximum number of seconds that can be set.
+  final int maxSeconds;
+
+  /// Build the widget.
+  @override
+  Widget build(final BuildContext context) {
+    final value = seconds ?? 0.0;
+    return CallbackShortcuts(
+      bindings: {
+        const SingleActivator(LogicalKeyboardKey.home): () => onChanged(null),
+        const SingleActivator(LogicalKeyboardKey.end): () =>
+            onChanged(maxSeconds.toDouble()),
+        const SingleActivator(LogicalKeyboardKey.pageUp): () =>
+            onChanged(min(maxSeconds.toDouble(), value + 1.0)),
+        const SingleActivator(LogicalKeyboardKey.pageDown): () {
+          final newValue = value - 1.0;
+          onChanged(newValue < 0 ? null : newValue);
+        },
+      },
+      child: Semantics(
+        container: true,
+        label: seconds == null
+            ? unsetMessage
+            : '${value.toStringAsFixed(1)} $secondsMessage',
+        child: Slider(
+          value: value,
+          onChanged: (final value) => onChanged(value == 0.0 ? null : value),
+          divisions: maxSeconds * 10,
+          label: secondsMessage,
+          max: maxSeconds.toDouble(),
+        ),
+      ),
+    );
+  }
+}
