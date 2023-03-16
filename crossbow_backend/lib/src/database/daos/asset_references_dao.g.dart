ce5e1eac485e262149c5bfcc3ddc757a15f35ae2
Chris Norman <chris.norman2@googlemail.com>
2023-03-16 07:11:02 +0000

    Updated Synthizer.

--- crossbow_backend/lib/src/database/daos/asset_references_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'asset_references_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$AssetReferencesDaoMixin on DatabaseAccessor<CrossbowBackendDatabase> {
-  $AssetReferencesTable get assetReferences => attachedDatabase.assetReferences;
-}
