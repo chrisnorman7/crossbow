bd87746dc198a4a0eec253b2039575e1c6176c4b
Chris Norman <chris.norman2@googlemail.com>
2023-05-15 07:00:13 +0100

    Make it possible to infer project settings.

--- crossbow_backend/bin/build_game.dart
+++ crossbow_backend/bin/build_game.dart
@@ -1,18 +1,30 @@
 // ignore_for_file: avoid_print
 import 'dart:io';
 
+import 'package:crossbow_backend/crossbow_backend.dart';
 import 'package:crossbow_backend/src/code_gen/project_code.dart';
 import 'package:dart_style/dart_style.dart';
 
 Future<void> main(final List<String> args) async {
-  if (args.length != 2) {
-    return print(
-      'You must specify a project file to load and an output directory.',
-    );
-  }
   final stopWatch = Stopwatch()..start();
-  final oldProjectFile = File(args.first);
-  final newProjectDirectory = Directory(args.last);
+  final File oldProjectFile;
+  final Directory newProjectDirectory;
+  if (args.length > 2) {
+    print('Too many arguments.');
+    print('At most, input_file and output_directory can be specified.');
+    return;
+  } else if (args.length > 1) {
+    newProjectDirectory = Directory(args.last);
+    oldProjectFile = File(args.first);
+  } else if (args.length == 1) {
+    oldProjectFile = File(args.first);
+    final project = Project.fromFile(oldProjectFile);
+    newProjectDirectory = Directory(project.appName);
+  } else {
+    oldProjectFile = File('project.json');
+    final project = Project.fromFile(oldProjectFile);
+    newProjectDirectory = Directory(project.appName);
+  }
   print(
     'Importing project ${oldProjectFile.path} into '
     '${newProjectDirectory.path}.',
